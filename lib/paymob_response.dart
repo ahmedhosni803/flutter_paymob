21917819baa975e8c5bd3fe42ea9423e5753cc0f
ahmed <ahmedhosni803@gmail.com>
2025-08-13 10:56:09 +0300

    version 1.0.8

--- lib/paymob_response.dart
+++ lib/paymob_response.dart
@@ -3,20 +3,29 @@ class PaymentPaymobResponse {
   String? transactionID;
   String? responseCode;
   String? message;
+  int? amountCents;
+  String? dataMessage;
 
-  PaymentPaymobResponse({
-    required this.success,
-    this.transactionID,
-    this.responseCode,
-    this.message,
-  });
+  PaymentPaymobResponse(
+      {required this.success,
+      this.transactionID,
+      this.responseCode,
+      this.amountCents,
+      this.message,
+      this.dataMessage});
 
   factory PaymentPaymobResponse.fromJson(Map<String, dynamic> json) {
     return PaymentPaymobResponse(
       success: json['success'].toString() == "true",
       transactionID: json['id'],
-      message: json['message'],
+      message: json['message'] ?? json['data.message'] != null
+          ? Uri.decodeComponent(json['data.message']!)
+          : '',
       responseCode: json['txn_response_code'],
+      amountCents: int.tryParse(json['amount_cents'] ?? '0') ?? 0,
+      dataMessage: json['data.message'] != null
+          ? Uri.decodeComponent(json['data.message']!)
+          : '',
     );
   }
 }
